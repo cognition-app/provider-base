09b2edf1d544b1b6e28e9c967c248a29f99a66cb
Daniel Clarke <u8sand@gmail.com>
2018-12-08 17:45:13 -0500

    Added simpler, working, straightforward generator examples

--- src/dummy/provider.ts
+++ src/dummy/provider.ts
@@ -1,9 +1,3 @@
-/**
- * Note: The challenge here is that iterators need to be able to mutate--that-is
- *  if it has changed since the last time, re-calling next() should still result
- *  in the next value.
- */
-
 import { BaseProvider, BaseProviderInstance } from '../base/provider'
 import { MaybeId, KeyVal } from '../base/types'
 
@@ -33,52 +27,76 @@ class DummyProviderInstance implements BaseProviderInstance<
     delete this._store[k]
   }
 
-  *forward(opts?: {gt?: string, ge?: string}): Iterator<KeyVal<string, Promise<object>>> {
-    let sortedKeys = Object.keys(this._store).sort()
-    if (opts !== undefined) {
-      if (opts.gt !== undefined) {
-        let ind = [...sortedKeys, opts.gt].sort().indexOf(opts.gt)
-        if(sortedKeys[ind] === opts.gt)
-          sortedKeys = sortedKeys.slice(ind + 1)
-        else
-          sortedKeys = sortedKeys.slice(ind + 1)
-      } else if (opts.ge !== undefined) {
-        let ind = [...sortedKeys, opts.ge].sort().indexOf(opts.ge)
-        if (sortedKeys[ind] === opts.ge)
-          sortedKeys = sortedKeys.slice(ind)
-        else
-          sortedKeys = sortedKeys.slice(ind)
-      }
-    }
+  // *forward(opts?: {gt?: string, ge?: string}): Iterator<KeyVal<string, Promise<object>>> {
+  //   let sortedKeys = Object.keys(this._store).sort()
+  //   if (opts !== undefined) {
+  //     if (opts.gt !== undefined) {
+  //       let ind = [...sortedKeys, opts.gt].sort().indexOf(opts.gt)
+  //       if(sortedKeys[ind] === opts.gt)
+  //         sortedKeys = sortedKeys.slice(ind + 1)
+  //       else
+  //         sortedKeys = sortedKeys.slice(ind + 1)
+  //     } else if (opts.ge !== undefined) {
+  //       let ind = [...sortedKeys, opts.ge].sort().indexOf(opts.ge)
+  //       if (sortedKeys[ind] === opts.ge)
+  //         sortedKeys = sortedKeys.slice(ind)
+  //       else
+  //         sortedKeys = sortedKeys.slice(ind)
+  //     }
+  //   }
 
-    for(const key of sortedKeys) {
-      yield {key, value: this.get(key)}
+  //   for(const key of sortedKeys) {
+  //     yield {key, value: this.get(key)}
+  //   }
+  // }
+  *forward(opts?: { gt?: string, ge?: string }): Iterator<KeyVal<string, Promise<object>>> {
+    for (const key of Object.keys(this._store).sort()) {
+      if(opts !== undefined) {
+        if(opts.gt !== undefined) {
+          if (key <= opts.gt) continue
+        } else if(opts.ge !== undefined) {
+          if (key < opts.ge) continue
+        }
+      }
+      yield { key, value: this.get(key) }
     }
   }
 
   *reverse(opts?: { lt?: string, le?: string }): Iterator<KeyVal<string, Promise<object>>> {
-    let sortedKeys = Object.keys(this._store).sort()
-    if (opts !== undefined) {
-      if (opts.lt !== undefined) {
-        let ind = [...sortedKeys, opts.lt].sort().indexOf(opts.lt)
-        if (sortedKeys[ind] === opts.lt)
-          sortedKeys = sortedKeys.slice(0, ind)
-        else
-          sortedKeys = sortedKeys.slice(0, ind)
-      } else if (opts.le !== undefined) {
-        let ind = [...sortedKeys, opts.le].sort().indexOf(opts.le)
-        if (sortedKeys[ind] === opts.le)
-          sortedKeys = sortedKeys.slice(0, ind + 1)
-        else
-          sortedKeys = sortedKeys.slice(0, ind)
+    for (const key of Object.keys(this._store).sort().reverse()) {
+      if (opts !== undefined) {
+        if (opts.lt !== undefined) {
+          if (key >= opts.lt) continue
+        } else if (opts.le !== undefined) {
+          if (key > opts.le) continue
+        }
       }
-    }
-    sortedKeys = sortedKeys.reverse()
-
-    for (const key of sortedKeys) {
       yield { key, value: this.get(key) }
     }
   }
+  // *reverse(opts?: { lt?: string, le?: string }): Iterator<KeyVal<string, Promise<object>>> {
+  //   let sortedKeys = Object.keys(this._store).sort()
+  //   if (opts !== undefined) {
+  //     if (opts.lt !== undefined) {
+  //       let ind = [...sortedKeys, opts.lt].sort().indexOf(opts.lt)
+  //       if (sortedKeys[ind] === opts.lt)
+  //         sortedKeys = sortedKeys.slice(0, ind)
+  //       else
+  //         sortedKeys = sortedKeys.slice(0, ind)
+  //     } else if (opts.le !== undefined) {
+  //       let ind = [...sortedKeys, opts.le].sort().indexOf(opts.le)
+  //       if (sortedKeys[ind] === opts.le)
+  //         sortedKeys = sortedKeys.slice(0, ind + 1)
+  //       else
+  //         sortedKeys = sortedKeys.slice(0, ind)
+  //     }
+  //   }
+  //   sortedKeys = sortedKeys.reverse()
+
+  //   for (const key of sortedKeys) {
+  //     yield { key, value: this.get(key) }
+  //   }
+  // }
 }
 
 export const DummyProvider = DummyProviderInstance as BaseProvider<string, object, undefined>
